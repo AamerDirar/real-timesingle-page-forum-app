b8cec57458c985555b6a58b9f394c92b4aa05a3a
unknown <aamer.idris91@gmail.com>
2021-06-12 10:21:06 +0300

    CRUD for Question along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -5,9 +5,24 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use App\User;
 
+
 class Question extends Model
 {
-    public function user() 
+    //protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
+
+    public function user()
     {
         return $this->belongSTo(User::class);
     }
